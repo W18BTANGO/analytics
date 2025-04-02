2d240be7b00476c7c43c2e6480e2671fc9c4aca2
katherine-gibson <katherinegibson999@gmail.com>
2025-04-02 16:15:10 +1100

    testing out automated deployment to ecs

--- tests/test_app.py
+++ tests/test_app.py
@@ -329,4 +329,4 @@ def test_most_expensive_and_cheapest_suburb_no_data():
 def test_health():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"status": "healthy","microservice": "analytics"}
+    assert response.json() == {"status": "healthy", "microservice": "analytics","updated":"02/04/2025"}
